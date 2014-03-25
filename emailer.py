e6f89c0ec8eadc2f66b6716be9c6b1032755ab53
slamice <slamice@gmail.com>
2014-03-26 00:19:27 +0900

    added subject method, test yet to be finished

--- emailer.py
+++ emailer.py
@@ -15,3 +15,12 @@ def create_addresses(from_addr,to_addr,cc=[],bcc=[]):
 
 def check_email_valid(email):
     return re.match(r"[^@]+@[^@]+\.[^@]+",email) != None
+
+def add_subject_to_email(msg,subject):
+    try:
+        msg['Subject'] = subject
+    except Exception,e:
+        print str(e)
+
+def add_text_to_email(msg,text):
+    try
--- test/emailer_test.py
+++ test/emailer_test.py
@@ -5,7 +5,6 @@ sys.path.append('..')
 import emailer
 
 def test_create_addresses_just_to_address():
-    print emailer.create_addresses('me@example.com','you@example.com')
     assert emailer.create_addresses('me@example.com','you@example.com') == ''
 
 def test_check_email_valid_empty_email():
@@ -15,7 +14,10 @@ def test_check_email_valid_real_email():
     assert emailer.check_email_valid('user@example.com') == True
 
 def test_add_subject_to_email():
-    assert emailer.add_subject_to_email(msg,subject) == 
+    msg = MIMEMultipart()
+    subject = 'This is an awesome subject line'
+    result = emailer.add_subject_to_email(msg,subject)
+    assert result == 
 
 def test_add_text_to_email():
     assert emailer.add_text_to_email(msg,text) == 
